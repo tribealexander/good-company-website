a4fe63178c380360d68baa7a3b6b3337a553ed1a
Alex Tribe <alex@tribealexander.com>
2026-02-23 17:36:03 -0500

    Fix technician case study thumbnail (use hqdefault)

--- src/lib/case-studies.ts
+++ src/lib/case-studies.ts
@@ -21,7 +21,7 @@ export const LOCAL_CASE_STUDIES: CaseStudy[] = [
     ],
     videoUrl: "https://youtu.be/TgYH2Vi-yrM",
     videoType: "youtube",
-    thumbnailUrl: "https://img.youtube.com/vi/TgYH2Vi-yrM/maxresdefault.jpg",
+    thumbnailUrl: "https://img.youtube.com/vi/TgYH2Vi-yrM/hqdefault.jpg",
     featured: false,
     order: 1,
   },
